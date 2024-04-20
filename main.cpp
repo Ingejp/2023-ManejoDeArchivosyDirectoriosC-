7a4f5d702265911246ea4727f1735d5114f18249
JP <ingejp@outlook.com>
2024-04-20 09:54:50 -0600

    Se corrige un error por llave comentada

--- main.cpp
+++ main.cpp
@@ -154,22 +154,3 @@ void renombrar(){
     }*/
 }
 
-string generarCadenaAleatoria(int longitud) {
-  /*  string cadena;
-    cadena.reserve(longitud); // Reservar espacio para evitar realocaciones frecuentes
-
-    // caracteres permitidos
-    const std::string caracteresPermitidos = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-    // generador de números aleatorios
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> distribucion(0, caracteresPermitidos.size() - 1);
-
-
-    for (int i = 0; i < longitud; ++i) {
-        cadena += caracteresPermitidos[distribucion(gen)];
-    }
-
-    return cadena;*/
-}
